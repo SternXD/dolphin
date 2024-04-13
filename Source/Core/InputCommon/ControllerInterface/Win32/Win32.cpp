bdf12dd77e57ca096f08994ddbd5fa5b012ae387
Tilka <tilkax@gmail.com>
2024-04-13 11:56:27 +0100

    Merge pull request #12632 from jordan-woyak/input-backend-impls

--- Source/Core/InputCommon/ControllerInterface/Win32/Win32.cpp
+++ Source/Core/InputCommon/ControllerInterface/Win32/Win32.cpp
@@ -20,12 +20,24 @@
 
 #pragma comment(lib, "OneCoreUAP.Lib")
 
-// Dolphin's render window
-static HWND s_hwnd;
 static std::mutex s_populate_mutex;
 // TODO is this really needed?
 static Common::Flag s_first_populate_devices_asked;
 static HCMNOTIFICATION s_notify_handle;
+
+namespace ciface::Win32
+{
+class InputBackend final : public ciface::InputBackend
+{
+public:
+  InputBackend(ControllerInterface* controller_interface);
+  ~InputBackend();
+
+  void PopulateDevices() override;
+  void HandleWindowChange() override;
+  HWND GetHWND();
+};
+}  // namespace ciface::Win32
 
 _Pre_satisfies_(EventDataSize >= sizeof(CM_NOTIFY_EVENT_DATA)) static DWORD CALLBACK
     OnDevicesChanged(_In_ HCMNOTIFICATION hNotify, _In_opt_ PVOID Context,
@@ -43,10 +55,11 @@ _Pre_satisfies_(EventDataSize >= sizeof(CM_NOTIFY_EVENT_DATA)) static DWORD CALL
       std::lock_guard lk_population(s_populate_mutex);
       // TODO: we could easily use the message passed alongside this event, which tells
       // whether a device was added or removed, to avoid removing old, still connected, devices
-      g_controller_interface.PlatformPopulateDevices([] {
-#ifndef WINRT_XBOX
-        ciface::DInput::PopulateDevices(s_hwnd);
-#endif
+      g_controller_interface.PlatformPopulateDevices([&] {
+      #ifndef WINRT_XBOX
+        ciface::DInput::PopulateDevices(
+            static_cast<ciface::Win32::InputBackend*>(Context)->GetHWND());
+      #endif
         ciface::XInput::PopulateDevices();
       });
     }
@@ -54,12 +67,21 @@ _Pre_satisfies_(EventDataSize >= sizeof(CM_NOTIFY_EVENT_DATA)) static DWORD CALL
   return ERROR_SUCCESS;
 }
 
-void ciface::Win32::Init(void* hwnd)
+namespace ciface::Win32
 {
-#ifndef WINRT_XBOX
-  s_hwnd = static_cast<HWND>(hwnd);
-#endif
+std::unique_ptr<ciface::InputBackend> CreateInputBackend(ControllerInterface* controller_interface)
+{
+  return std::make_unique<InputBackend>(controller_interface);
+}
 
+HWND InputBackend::GetHWND()
+{
+  return static_cast<HWND>(GetControllerInterface().GetWindowSystemInfo().render_window);
+}
+
+InputBackend::InputBackend(ControllerInterface* controller_interface)
+    : ciface::InputBackend(controller_interface)
+{
   XInput::Init();
   WGInput::Init();
 
@@ -67,42 +89,37 @@ void ciface::Win32::Init(void* hwnd)
                                  .FilterType = CM_NOTIFY_FILTER_TYPE_DEVICEINTERFACE,
                                  .u{.DeviceInterface{.ClassGuid = GUID_DEVINTERFACE_HID}}};
   const CONFIGRET cfg_rv =
-      CM_Register_Notification(&notify_filter, nullptr, OnDevicesChanged, &s_notify_handle);
+      CM_Register_Notification(&notify_filter, this, OnDevicesChanged, &s_notify_handle);
   if (cfg_rv != CR_SUCCESS)
   {
     ERROR_LOG_FMT(CONTROLLERINTERFACE, "CM_Register_Notification failed: {:x}", cfg_rv);
   }
 }
 
-void ciface::Win32::PopulateDevices(void* hwnd)
+void InputBackend::PopulateDevices()
 {
-#ifndef WINRT_XBOX
-  s_hwnd = static_cast<HWND>(hwnd);
-#endif
-
   std::lock_guard lk_population(s_populate_mutex);
   s_first_populate_devices_asked.Set();
 #ifndef WINRT_XBOX
-  ciface::DInput::PopulateDevices(s_hwnd);
+  ciface::DInput::PopulateDevices(GetHWND());
 #endif
   ciface::XInput::PopulateDevices();
   ciface::WGInput::PopulateDevices();
 }
 
-void ciface::Win32::ChangeWindow(void* hwnd)
+void InputBackend::HandleWindowChange()
 {
-#ifndef WINRT_XBOX
+  #ifndef WINRT_XBOX
   s_hwnd = static_cast<HWND>(hwnd);
   std::lock_guard lk_population(s_populate_mutex);
-  ciface::DInput::ChangeWindow(s_hwnd);
-#endif
+  ciface::DInput::ChangeWindow(GetHWND());
+  #endif
 }
 
-void ciface::Win32::DeInit()
+InputBackend::~InputBackend()
 {
   s_first_populate_devices_asked.Clear();
   DInput::DeInit();
-  s_hwnd = nullptr;
 
   if (s_notify_handle)
   {
@@ -117,3 +134,5 @@ void ciface::Win32::DeInit()
   XInput::DeInit();
   WGInput::DeInit();
 }
+
+}  // namespace ciface::Win32
